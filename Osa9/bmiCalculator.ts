a311022d40ad0201ccc4212b2426eb588e510095
Olli Tanhuanpää <ottanh@utu.fi>
2022-04-29 21:06:00 +0300

    9.1-9.3

---
+++ Osa9/bmiCalculator.ts
@@ -0,0 +1,25 @@
+const parseArgumentsBmi = (args: Array<string>): [number, number] => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return [Number(args[2]), Number(args[3])];
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+const calculateBmi = (heigth: number, weigth: number): string => {
+  const bmi =  weigth / (heigth/100)**2
+  console.log(bmi)
+  if(bmi < 18.5){
+    return 'Underweight'
+  } else if (bmi > 25){
+    return 'Overweight'
+  } else {
+    return 'Normal'
+  }
+} 
+
+const [heigth, weigth] : [number, number] = parseArgumentsBmi(process.argv);
+console.log(calculateBmi(heigth, weigth));
---
+++ Osa9/exerciseCalculator.ts
@@ -0,0 +1,65 @@
+interface Result { 
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string,
+  target: number,
+  average: number
+}
+
+const parseArguments = (args: Array<string>): [Array<number>,number] => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+
+  let target: number;
+  if (!isNaN(Number(args[2])) ) {
+    target = Number(args[2]);
+  } else {
+    throw new Error('All values must be numbers!');
+  }
+
+  const hours: Array<number> = [];
+  for (let i: number = 3; i < args.length; i++) {
+    const value: number = Number(args[i]);
+    if(!isNaN(value)) {
+      hours.push(value);
+    } else {
+      throw new Error('All values must be numbers!');
+    }
+  }
+
+  return [hours, target]
+}
+
+
+const calculateExercises = (hours: Array<number>, target: number): Result => {
+  const periodLength: number = hours.length;
+  const trainingDays: number = hours.filter(h => h !== 0).length;
+  const average: number = hours.reduce((p, c) => p + c, 0) / hours.length;
+
+  const ratingDescriptions: Array<string> = ['Not very good', 'Okay but could be better', 'Very good']
+
+  let rating: number;
+  if(average < (target/2)) {
+    rating = 1;
+  } else if (average < target) {
+    rating = 2;
+  } else {
+    rating = 3;
+  }
+  
+  return {
+    periodLength,
+    trainingDays,
+    success: average >= target,
+    rating,
+    ratingDescription: ratingDescriptions[rating-1],
+    target,
+    average
+  }
+}
+
+
+
+const [hours, target] : [Array<number>, number] = parseArguments(process.argv);
+console.log(calculateExercises(hours, target));
